17aa388c0b06c77349d59b522403ab36cecb2501
Ezz Eldin Ayman <ezz_ayman@hotmail.com>
2022-03-04 20:25:01 +0200

    New Private Safe Project

---
+++ PrivateSafe/PrivateSafe.c
@@ -0,0 +1,17 @@
+/*
+ * PrivateSafe.c
+ *
+ * Created: 04-Mar-22 7:52:35 PM
+ *  Author: ezz_a
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
